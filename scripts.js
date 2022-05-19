c469eb106735c5bda4c3442bcad4777f29bdc61e
JP <jonpaulvd@hotmail.com>
2022-05-19 00:58:38 -0700

    troublehshoot code

--- scripts.js
+++ scripts.js
@@ -8,9 +8,25 @@
 // }
 
 
+let el = document.querySelector("#button-1")
+el.addEventListener("click", myFunction);
 
+let el2 = document.querySelector("#button-2")
+el2.addEventListener("click", myFunction);
 
-function changeColor(e) {
-   console.log(e)
-    console.log("running changeColor Function")
+let el3 = document.querySelector("#button-3")
+el3.addEventListener("click", myFunction);
+
+let el4 = document.querySelector("#button-4")
+el4.addEventListener("click", myFunction);
+
+let el5 = document.querySelector("#button-5")
+el5.addEventListener("click", myFunction);
+
+let el6 = document.querySelector("#button-6")
+el6.addEventListener("click", myFunction);
+
+function myFunction(e){
+  console.log(this.value);
 }
+
