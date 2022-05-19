1d6a4a37688471bf0c64a929551c393caafafaf7
JP <jonpaulvd@hotmail.com>
2022-05-19 01:19:15 -0700

    addressed obj load error

--- scripts.js
+++ scripts.js
@@ -6,9 +6,8 @@
 //     document.head.appendChild(m);
 //     console.log(m,m.content);
 // }
-
-
-let el = document.querySelector("#button-1")
+window.addEventListener('DOMContentLoaded', (event) => {
+    let el = document.querySelector("#button-1")
 el.addEventListener("click", changeColor);
 
 let el2 = document.querySelector("#button-2")
@@ -26,10 +25,30 @@ el5.addEventListener("click", changeColor);
 let el6 = document.querySelector("#button-6")
 el6.addEventListener("click", changeColor);
 
+});
+
+
 function changeColor(e){
   console.log(this.value);
   console.log(formatCache)
 
+   for (let i = 0; i < formatCache.length; i++)
+   {
+
+     let type = formatCache[i].type;
+     let elementName = formatCache[i].element; 
+     let prefix = '';
+     if (type == "class")
+     prefix = '.'
+     else
+     prefix = '#'
+     let str = prefix+elementName
+     console.log(str)
+     let forElement = document.querySelector(str)
+     console.log(forElement)
+     //forElement.style.background = this.value;
+
+   }
 
 
 
@@ -39,4 +58,4 @@ function changeColor(e){
 
 }
 
-var formatCache = [ {type: "class", element: 'html'}, {type: "class", element: 'body'},{type: "class", element: 'main'}, {type: "class", element: 'yg51vc'}, {type: "class", element: 'LX3sZb'}, {type: "class", element:'KxwPGc.SSwjIe'}, {type: "class", element: 'dodTBe'}, {type: "class", element: 'o3j99.n1xJcf.Ne6nSd'},{type: "class", element: 'yg51vc'}, {type: "class", element:'o3j99.n1xJcf.Ne6nSd'},{type: "class", element: 'CvDJxb'}, {type:"class", element:'GLcBOb.mCRcc'},{type: "class", element:  'WE0UJf'}, {type:"class", element:'ZGomKf'},{type: "class", element: 'J1hqV'}, {type: "class", element:'sfbg'},{type: "class", element: '#footcnt'}]
+var formatCache = [ {type: "class", element: 'html'}, {type: "class", element: 'body'},{type: "class", element: 'main'}, {type: "class", element: 'yg51vc'}, {type: "class", element: 'LX3sZb'}, {type: "class", element:'KxwPGc.SSwjIe'}, {type: "class", element: 'dodTBe'}, {type: "class", element: 'o3j99.n1xJcf.Ne6nSd'},{type: "class", element: 'yg51vc'}, {type: "class", element:'o3j99.n1xJcf.Ne6nSd'},{type: "class", element: 'CvDJxb'}, {type:"class", element:'GLcBOb.mCRcc'},{type: "class", element:  'WE0UJf'}, {type:"class", element:'ZGomKf'},{type: "class", element: 'J1hqV'}, {type: "class", element:'sfbg'},{type: "id", element: 'footcnt'}]
