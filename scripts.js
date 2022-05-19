25c595265cbb4da3b5d2ef6c482044004afb5c38
JP <jonpaulvd@hotmail.com>
2022-05-19 01:00:50 -0700

    troublehshoot code, refined and added cache for formatting

--- scripts.js
+++ scripts.js
@@ -28,5 +28,7 @@ el6.addEventListener("click", myFunction);
 
 function myFunction(e){
   console.log(this.value);
+  console.log(formatCache)
 }
 
+var formatCache = [ {type: "class", element: 'html'}, {type: "class", element: 'body'},{type: "class", element: 'main'}, {type: "class", element: 'yg51vc'}, {type: "class", element: 'LX3sZb'}, {type: "class", element:'KxwPGc.SSwjIe'}, {type: "class", element: 'dodTBe'}, {type: "class", element: 'o3j99.n1xJcf.Ne6nSd'},{type: "class", element: 'yg51vc'}, {type: "class", element:'o3j99.n1xJcf.Ne6nSd'},{type: "class", element: 'CvDJxb'}, {type:"class", element:'GLcBOb.mCRcc'},{type: "class", element:  'WE0UJf'}, {type:"class", element:'ZGomKf'},{type: "class", element: 'J1hqV'}, {type: "class", element:'sfbg'},{type: "class", element: '#footcnt'}]
