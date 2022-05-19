64f908e59a986148f0fbd05eb4576c926be74471
JP <jonpaulvd@hotmail.com>
2022-05-19 01:02:31 -0700

    troublehshoot code, refined and added cache for formatting w/ fn call

--- scripts.js
+++ scripts.js
@@ -9,26 +9,34 @@
 
 
 let el = document.querySelector("#button-1")
-el.addEventListener("click", myFunction);
+el.addEventListener("click", changeColor);
 
 let el2 = document.querySelector("#button-2")
-el2.addEventListener("click", myFunction);
+el2.addEventListener("click", changeColor);
 
 let el3 = document.querySelector("#button-3")
-el3.addEventListener("click", myFunction);
+el3.addEventListener("click", changeColor);
 
 let el4 = document.querySelector("#button-4")
-el4.addEventListener("click", myFunction);
+el4.addEventListener("click", changeColor);
 
 let el5 = document.querySelector("#button-5")
-el5.addEventListener("click", myFunction);
+el5.addEventListener("click", changeColor);
 
 let el6 = document.querySelector("#button-6")
-el6.addEventListener("click", myFunction);
+el6.addEventListener("click", changeColor);
 
-function myFunction(e){
+function changeColor(e){
   console.log(this.value);
   console.log(formatCache)
+
+
+
+
+
+
+
+
 }
 
 var formatCache = [ {type: "class", element: 'html'}, {type: "class", element: 'body'},{type: "class", element: 'main'}, {type: "class", element: 'yg51vc'}, {type: "class", element: 'LX3sZb'}, {type: "class", element:'KxwPGc.SSwjIe'}, {type: "class", element: 'dodTBe'}, {type: "class", element: 'o3j99.n1xJcf.Ne6nSd'},{type: "class", element: 'yg51vc'}, {type: "class", element:'o3j99.n1xJcf.Ne6nSd'},{type: "class", element: 'CvDJxb'}, {type:"class", element:'GLcBOb.mCRcc'},{type: "class", element:  'WE0UJf'}, {type:"class", element:'ZGomKf'},{type: "class", element: 'J1hqV'}, {type: "class", element:'sfbg'},{type: "class", element: '#footcnt'}]
