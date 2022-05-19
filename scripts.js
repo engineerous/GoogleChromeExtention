f322c1d8ce884e27f6aead9bfe7b8478ae58389a
Peng Dong <dongpeng101@gmail.com>
2022-05-19 01:01:45 -0400

    adding feature

---
+++ scripts.js
@@ -0,0 +1,11 @@
+//grey: '#152126', '#152126'
+// window.onload = () => {
+//     var m = document.createElement('meta'); 
+//     m.name = 'theme-color'; 
+//     m.content = '#152126'; 
+//     document.head.appendChild(m);
+//     console.log(m,m.content);
+// }
+
+
+
